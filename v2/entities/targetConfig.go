837bc9b2ea2ef77a3252c962a80ba7341f384428
Philipp Pixel <boris.schaa@cloudogu.com>
2025-01-20 15:40:13 +0100

    #1 Implement missing deep-copy functions for TargetConfig
    Somehow these functions seem to be necessary by the blueprint operator's
    deep copy generator even though I do not see where I might have deleted
    it

--- v2/entities/targetConfig.go
+++ v2/entities/targetConfig.go
@@ -1,12 +1,35 @@
 package entities
 
+import "encoding/json"
+
 // TargetConfig contains Dogu and Cloudogu EcoSystem specific configuration data which determine set-up and run
 // behaviour respectively.
 type TargetConfig struct {
 	// Dogus contains Dogu specific configuration data which determine set-up and run behaviour.
-	Dogus map[string]CombinedDoguConfig `json:"dogus,omitempty"`
+	Dogus DoguConfigMap `json:"dogus,omitempty"`
 	// Dogus contains Cloudogu EcoSystem specific configuration data which determine set-up and run behaviour.
 	Global GlobalConfig `json:"global,omitempty"`
+}
+
+type DoguConfigMap map[string]CombinedDoguConfig
+
+func (in *DoguConfigMap) DeepCopy() *DoguConfigMap {
+	out := new(DoguConfigMap)
+	in.DeepCopyInto(out)
+	return out
+}
+
+func (in *DoguConfigMap) DeepCopyInto(out *DoguConfigMap) {
+	if out != nil {
+		jsonStr, err := json.Marshal(in)
+		if err != nil {
+			return
+		}
+		err = json.Unmarshal(jsonStr, in)
+		if err != nil {
+			return
+		}
+	}
 }
 
 // CombinedDoguConfig contains configuration data of different sensitivity.
@@ -37,3 +60,29 @@ type DoguConfigKey struct {
 }
 
 type DoguConfigValue string
+
+func (in TargetConfig) DeepCopyInto(out *TargetConfig) {
+	if out != nil {
+		out.Global = *in.Global.DeepCopy()
+		out.Dogus = *in.Dogus.DeepCopy()
+	}
+}
+
+func (in *GlobalConfig) DeepCopy() *GlobalConfig {
+	out := new(GlobalConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+func (in *GlobalConfig) DeepCopyInto(out *GlobalConfig) {
+	if out != nil {
+		jsonStr, err := json.Marshal(in)
+		if err != nil {
+			return
+		}
+		err = json.Unmarshal(jsonStr, in)
+		if err != nil {
+			return
+		}
+	}
+}
