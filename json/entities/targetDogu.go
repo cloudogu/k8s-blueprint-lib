229abb53849db2f11ca5603c01c1df36369f88f2
Sven Gürtler <sven.guertler@cloudogu.com>
2025-05-14 15:23:44 +0200

    #3 add fields for additional mounts

--- json/entities/targetDogu.go
+++ json/entities/targetDogu.go
@@ -23,7 +23,34 @@ type ReverseProxyConfig struct {
 	AdditionalConfig string `json:"additionalConfig,omitempty"`
 }
 
+type DataSourceType string
+
+//goland:noinspection GoUnusedConst
+const (
+	// DataSourceConfigMap mounts a config map as a data source.
+	DataSourceConfigMap DataSourceType = "ConfigMap"
+	// DataSourceSecret mounts a secret as a data source.
+	DataSourceSecret DataSourceType = "Secret"
+)
+
+// AdditionalMount is a description of what data should be mounted to a specific Dogu volume (already defined in dogu.json).
+type AdditionalMount struct {
+	// SourceType defines where the data is coming from.
+	// Valid options are:
+	//   ConfigMap - data stored in a kubernetes ConfigMap.
+	//   Secret - data stored in a kubernetes Secret.
+	SourceType DataSourceType `json:"sourceType"`
+	// Name is the name of the data source.
+	Name string `json:"name"`
+	// Volume is the name of the volume to which the data should be mounted. It is defined in the respective dogu.json.
+	Volume string `json:"volume"`
+	// Subfolder defines a subfolder in which the data should be put within the volume.
+	// +optional
+	Subfolder string `json:"subfolder,omitempty"`
+}
+
 type PlatformConfig struct {
-	ResourceConfig     ResourceConfig     `json:"resource,omitempty"`
-	ReverseProxyConfig ReverseProxyConfig `json:"reverseProxy,omitempty"`
+	ResourceConfig         ResourceConfig     `json:"resource,omitempty"`
+	ReverseProxyConfig     ReverseProxyConfig `json:"reverseProxy,omitempty"`
+	AdditionalMountsConfig []AdditionalMount  `json:"additionalMounts,omitempty" patchStrategy:"replace"`
 }
