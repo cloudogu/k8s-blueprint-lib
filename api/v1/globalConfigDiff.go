5e7b1efe680e246f73ce00871a04c8f23563af4d
alexander-dammeier <alexander.dammeier@cloudogu.com>
2025-06-03 11:20:26 +0200

    #3 add godoc for all CRD types

--- api/v1/globalConfigDiff.go
+++ api/v1/globalConfigDiff.go
@@ -1,8 +1,12 @@
 package v1
 
+// GlobalConfigDiff is a list of differences between the GlobalConfig in the EffectiveBlueprint and the cluster state
 type GlobalConfigDiff []GlobalConfigEntryDiff
 
+// GlobalConfigValueState represents either the actual or expected state of a global config key
 type GlobalConfigValueState ConfigValueState
+
+// GlobalConfigEntryDiff contains the difference and the needed actions for a single global config key
 type GlobalConfigEntryDiff struct {
 	Key          string                 `json:"key"`
 	Actual       GlobalConfigValueState `json:"actual"`
