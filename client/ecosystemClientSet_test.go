a140fb113f1cda2f6954739544c4841af280326b
Niklas <niklas.hinze@cloudogu.com>
2025-06-04 08:25:53 +0200

    #3 Add tests

--- client/ecosystemClientSet_test.go
+++ client/ecosystemClientSet_test.go
@@ -1,7 +1,10 @@
 package kubernetes
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"testing"
 )
@@ -35,4 +38,45 @@ func TestEcoSystemV1Alpha1Client_Dogus(t *testing.T) {
 		// then
 		require.NotNil(t, client)
 	})
+}
+
+func TestNewClientSet(t *testing.T) {
+	type args struct {
+		config    *rest.Config
+		clientSet *kubernetes.Clientset
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *ClientSet
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "should return error on nil config",
+			args: args{
+				config:    nil,
+				clientSet: nil,
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+		{
+			name: "should return error on nil clientSet",
+			args: args{
+				config:    &rest.Config{},
+				clientSet: nil,
+			},
+			want:    nil,
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewClientSet(tt.args.config, tt.args.clientSet)
+			if !tt.wantErr(t, err, fmt.Sprintf("NewClientSet(%v, %v)", tt.args.config, tt.args.clientSet)) {
+				return
+			}
+			assert.Equalf(t, tt.want, got, "NewClientSet(%v, %v)", tt.args.config, tt.args.clientSet)
+		})
+	}
 }
